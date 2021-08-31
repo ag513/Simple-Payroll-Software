6ffcd04bf303459a80bbd6f76c286dfecfb8f1da
Abhinav Gunishetty <abhinav.gunishetty@steinias.com>
2021-08-31 22:19:18 +0100

    Created staff class with properties

--- Payroll Software/Payroll Software/Program.cs
+++ Payroll Software/Payroll Software/Program.cs
@@ -9,4 +9,47 @@ namespace Payroll_Software
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        // Auto implemented properties
+        public float TotalPay { 
+            get 
+            {
+                return TotalPay;
+            }
+            protected set 
+            {
+                TotalPay = value;
+            } 
+        }
+        public float BasicPay
+        {
+            get
+            {
+                return BasicPay;
+            }
+            private set 
+            {
+                BasicPay = value;
+            }
+        }
+        public string NameOfStaff { get; set; }
+        public float HoursWOrked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (HoursWOrked > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
 }
